1db6f5d9d92b4e5cfec70f420c3d502192d49471
JE0903 <ehfehf79@gmail.com>
2018-01-17 16:22:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ def callback():
 
         data=conn.json()
        
-        data=data['PartDetails']['UnitPrice']
+        #data=data['PartDetails']['UnitPrice']
         data=json.dumps(data)
         return data
         
