43d2a38e7c2eaad407d4f7320cbfe0555b7afd3f
JE0903 <ehfehf79@gmail.com>
2018-01-17 13:55:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ from flask import make_response
 CLIENT_ID = "3f77a5f9-040a-4fc2-82b5-f33cbac4aec1"
 CLIENT_SECRET = "wY0nF1oV0xG7qQ0dC8dK2hB7wW4tW2rO4oI7pI3fN6oW7qH5yL"
 REDIRECT_URI = "https://digikeybot.herokuapp.com/callback"
-CODE="DtPMEWQQOFfErIKaUttx6Twlyom88gfoPKV1xQAE"
+CODE="ekRqly7x6gS0kv-ZW2vV6RcQrvCEKb3FyTumqgAD"
 # Flask app should start in global layout
 app = Flask(__name__)
 @app.route('/')
