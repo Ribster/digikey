95f50bf97b4ca848ef4158659ccd7d8cbcce3254
JE0903 <ehfehf79@gmail.com>
2018-01-17 14:29:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ def callback():
         data = res.read()
         data=data.decode("utf-8")
         
-        return data
+        return data['PartDetails']['UnitPrice']
         
         
 	
