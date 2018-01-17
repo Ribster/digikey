38250417e2bacd2b23f8d4d76c45f0e2a9b2aa59
JE0903 <ehfehf79@gmail.com>
2018-01-17 15:18:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -114,8 +114,8 @@ def callback():
         
         conn=requests.post("https://api.digikey.com/services/partsearch/v2/partdetails", data=payload, headers=headers)
 
-        data=conn.json()
-        data=json.loads(data)        
+        data=json.loads(conn)
+       
         data=data['PartDetails']['UnitPrice']
         data=json.dumps(data)
         return data
